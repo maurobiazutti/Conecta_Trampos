ee128dcd4d0e7b14dac96b886196f675a306fc51
Mauro <mauro_biazutti@hotmail.com>
2026-04-20 10:52:39 -0300

    feat: create registration profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,47 @@
+# app/controllers/profiles_controller.rb
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_profile, only: [ :show, :edit, :update ]
+
+  def show
+    redirect_to new_profile_path unless @profile
+  end
+
+  def new
+    redirect_to edit_profile_path if current_user.profile.present?
+    @profile = current_user.build_profile
+  end
+
+  def create
+    @profile = current_user.build_profile(profile_params)
+
+    if @profile.save
+      redirect_to profile_path, notice: "Perfil criado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @profile = current_user.profile
+    # @profile.build_user unless @profile.user
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: "Perfil atualizado!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:description, :phone, :active, user_attributes: [ :name ])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,18 @@ class User < ApplicationRecord
   #  :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable,
          :registerable,
-         :recoverable, 
-         :rememberable, 
+         :recoverable,
+         :rememberable,
          :validatable,
          :confirmable,
          :trackable
 
-  validates :name, presence: true
 
   has_one :address, dependent: :destroy
   has_one :profile, dependent: :destroy
   has_many :service_requests, dependent: :destroy
+
+  def profile_incomplete?
+    profile.nil? || profile.phone.blank? || profile.description.blank?
+  end
 end
